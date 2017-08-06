0395239d0f03fec396ff6c135793017761094ab7
Craig Disselkoen <cdisselk@eng.ucsd.edu>
2017-08-05 17:26:27 -0700

    Bunch of unrelated changes
    that probably should be split with rebase, but I'm lazy.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -13,6 +13,8 @@
 #include "Kaleidoscope-Macros.h"
 #include "Kaleidoscope-LEDControl.h"
 #include "Kaleidoscope-Numlock.h"
+#include "Kaleidoscope-OneShot.h"
+#include "Kaleidoscope-Escape-OneShot.h"
 #include "Kaleidoscope.h"
 
 #include "LED-Off.h"
@@ -22,7 +24,8 @@
 #include "Kaleidoscope-LEDEffect-Chase.h"
 #include "Kaleidoscope-LEDEffect-Rainbow.h"
 #include "Kaleidoscope-LED-Stalker.h"
-#include "Kaleidoscope-LED-AlphaSquare.h"
+#include "Kaleidoscope-LED-LetterGuesser.h"
+#include "Kaleidoscope-LED-ActiveModColor.h"
 #include "Kaleidoscope-Model01-TestMode.h"
 
 #define MACRO_VERSION_INFO 1
@@ -32,18 +35,18 @@
 #define NUMPAD_KEYMAP 2
 
 #define GENERIC_FN2  KEYMAP_STACKED ( \
-___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,         \
-Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE, \
-Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,                   \
-Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE, \
-                                            Key_LeftControl, Key_Delete, Key_LeftGui, Key_LeftShift,  \
+___,      Key_F1,           Key_F2,      Key_F3,        Key_F4,        Key_F5,           XXX,         \
+Key_Tab,  Key_mouseBtnR,    Key_mouseUp, Key_mouseBtnL, Key_mouseWarpNW, Key_mouseWarpNE, Key_PageDown, \
+Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR,    Key_mouseWarpSW, Key_mouseWarpSE,                   \
+Key_End,  Key_PrintScreen,  Key_Insert,  Key_mouseBtnM, Key_mouseWarpEnd, ___,            Key_PageUp, \
+                                            Key_Delete, OSM(LeftShift), OSM(LeftControl), ___,  \
                                                                 ___,   \
 \
 Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                  Key_F9,          Key_F10,          Key_F11, \
 Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,   Key_LeftBracket, Key_RightBracket, Key_F12, \
                             Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,             Key_RightArrow,  ___,              ___, \
 Key_PcApplication,          Key_Mute,               Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,            Key_Backslash,    Key_Pipe, \
-Key_RightShift, Key_RightAlt, Key_Enter, Key_RightControl, \
+OSM(LeftAlt), OSM(LeftGui), Key_Enter, Key_Escape, \
 ___ \
 )
 
@@ -60,25 +63,25 @@ ___ \
 \
     Macro_VersionInfo,  ___, Key_Keypad7, Key_Keypad8,   Key_Keypad9,        Key_KeypadSubtract, ___, \
     ___, ___, Key_Keypad4, Key_Keypad5,   Key_Keypad6,        Key_KeypadAdd,      ___, \
-         ___, Key_Keypad1, Key_Keypad2,   Key_Keypad3,        Key_Equals,         Key_Quote, \
-    ___, ___, Key_Keypad0, Key_KeypadDot, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter, \
+         ___, Key_Keypad1, Key_Keypad2,   Key_Keypad3,        Key_KeypadMultiply,         Key_Quote, \
+    ___, ___, Key_Keypad0, Key_Comma, Key_KeypadDot, Key_KeypadDivide,   Key_Enter, \
     ___, ___, ___, ___, \
     Key_Keymap1_Momentary \
 )
 
 #define QWERTY KEYMAP_STACKED ( \
     ___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext, \
-    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,           \
-    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,                    \
-    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,        \
-    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,         \
+    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Home,           \
+    Key_Tab,   Key_A, Key_S, Key_D, Key_F, Key_G,                    \
+    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_End,        \
+    Key_Backspace, OSM(LeftShift), OSM(LeftControl), OSL(2),         \
                           Key_Keymap1_Momentary,     \
 \
     Macro_Any,       Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_ToggleNumlock, \
     Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,       \
                    Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,       \
-    Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,       \
-    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,                   \
+    Key_LeftGui,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,       \
+    OSM(LeftAlt), OSM(LeftGui), Key_Spacebar, Key_Delete,                   \
     Key_Keymap1_Momentary \
 )
 
@@ -116,26 +119,27 @@ const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
 void setup() {
   Kaleidoscope.setup(KEYMAP_SIZE);
   BootKeyboard.begin();
-  Kaleidoscope.use(
-    &BootGreetingEffect,
-    &TestMode,
-    &LEDControl, &LEDOff,
-    &LEDRainbowEffect, &LEDRainbowWaveEffect, &LEDChaseEffect,
-    &solidRed, &solidOrange, &solidYellow, &solidGreen, &solidBlue, &solidIndigo, &solidViolet,
-    &LEDBreatheEffect,
-    &AlphaSquareEffect,
-    &StalkerEffect,
-    &NumLock,
-    &Macros,
-    &MouseKeys,
-    NULL);
+  Kaleidoscope.use(&BootGreetingEffect,
+                   &LEDControl, &LEDOff,
+                   &LEDRainbowEffect, &LEDRainbowWaveEffect, &LEDChaseEffect,
+                   &solidRed, &solidOrange, &solidYellow, &solidGreen, &solidBlue, &solidViolet,
+                   &LEDBreatheEffect,
+                   &LetterGuesserEffect,
+                   &StalkerEffect,
+                   &NumLock,
+
+                   &OneShot,
+                   &EscapeOneShot,
+                   &Macros,
+                   &MouseKeys,
+                   &ActiveModColorEffect,
+                   NULL);
 
   NumLock.numPadLayer = NUMPAD_KEYMAP;
-  AlphaSquare.color = { 255, 0, 0 };
   LEDRainbowEffect.brightness(150);
   LEDRainbowWaveEffect.brightness(150);
   StalkerEffect.variant = STALKER(BlazingTrail);
-  LEDOff.activate();
+  LEDRainbowEffect.activate();
 }
 
 
