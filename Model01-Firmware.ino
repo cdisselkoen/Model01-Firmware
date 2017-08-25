a99089c530d133a73c19d112ae70b2789e757968
Craig Disselkoen <cdisselk@eng.ucsd.edu>
2017-08-24 21:23:18 -0700

    Fix for upstream changes

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -91,13 +91,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
   NUMPAD
 };
 
-static kaleidoscope::LEDSolidColor solidRed(160, 0, 0);
-static kaleidoscope::LEDSolidColor solidOrange(140, 70, 0);
 static kaleidoscope::LEDSolidColor solidYellow(130, 100, 0);
-static kaleidoscope::LEDSolidColor solidGreen(0, 160, 0);
-static kaleidoscope::LEDSolidColor solidBlue(0, 70, 130);
-static kaleidoscope::LEDSolidColor solidIndigo(0, 0, 170);
-static kaleidoscope::LEDSolidColor solidViolet(130, 0, 120);
 
 const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
   if (macroIndex == TOGGLENUMLOCK && keyToggledOn(keyState)) {
