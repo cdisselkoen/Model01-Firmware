5a69d00ffba68f7d8cc84f1c4cac4e1583848a0c
Craig Disselkoen <cdisselk@eng.ucsd.edu>
2017-10-10 22:44:10 -0700

    Keymaps aren't macros
    This commit brings this branch closer to catching up with upstream

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -34,63 +34,54 @@
 #define Macro_Any M(MACRO_ANY)
 #define MACRO_LEDTOGGLE 3
 #define Macro_LEDToggle M(MACRO_LEDTOGGLE)
-#define NUMPAD_KEYMAP 2
 
-#define GENERIC_FN2  KEYMAP_STACKED ( \
-___,      Key_F1,           Key_F2,      Key_F3,        Key_F4,        Key_F5,           XXX,         \
-Key_Tab,  Key_mouseBtnR,    Key_mouseUp, Key_mouseBtnL, Key_mouseWarpNW, Key_mouseWarpNE, Key_PageDown, \
-Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR,    Key_mouseWarpSW, Key_mouseWarpSE,                   \
-Key_End,  Key_PrintScreen,  Key_Insert,  Key_mouseBtnM, Key_mouseWarpEnd, ___,            Key_PageUp, \
-                                          Key_Delete, OSM(LeftShift), OSM(LeftControl), ___,  \
-                                                              ___,   \
-\
-Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                  Key_F9,          Key_F10,          Key_F11, \
-Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,   Key_LeftParen,   Key_RightParen, Key_F12, \
-                            Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,             Key_RightArrow,  Key_Pipe,              ___, \
-Consumer_Mute,              Consumer_VolumeIncrement, Consumer_VolumeDecrement, Key_LeftBracket,       Key_RightBracket, Key_Backslash,    Key_Pipe, \
-OSM(LeftAlt), OSM(LeftGui), Key_Enter, Key_Escape, \
-___ \
-)
-
-
-
-#define NUMPAD KEYMAP_STACKED  (\
-    ___, ___, ___, ___, ___, ___, ___,  \
-    ___, ___, ___, ___, ___, ___, ___,  \
-    ___, ___, ___, ___, ___, ___,       \
-    ___, ___, ___, ___, ___, ___, ___,  \
-               ___, ___, ___, ___,  \
-                 Key_Keymap1_Momentary, \
-\
-\
-    Macro_VersionInfo,  ___, Key_Keypad7, Key_Keypad8,   Key_Keypad9,        Key_KeypadSubtract, ___, \
-    ___, ___, Key_Keypad4, Key_Keypad5,   Key_Keypad6,        Key_KeypadAdd,      ___, \
-         ___, Key_Keypad1, Key_Keypad2,   Key_Keypad3,        Key_KeypadMultiply,         Key_Quote, \
-    ___, ___, Key_Keypad0, Key_Comma, Key_KeypadDot, Key_KeypadDivide,   Key_Enter, \
-    ___, ___, ___, ___, \
-    Key_Keymap1_Momentary \
-)
-
-#define QWERTY KEYMAP_STACKED ( \
-    ___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext, \
-    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Macro_LEDToggle,   \
-    Key_Tab,      Key_A, Key_S, Key_D, Key_F, Key_G,                    \
-    Key_Home,     Key_Z, Key_X, Key_C, Key_V, Key_B, Key_End,        \
-    Key_Backspace, OSM(LeftShift), OSM(LeftControl), OSL(2),         \
-                          Key_Keymap1_Momentary,     \
-\
-    Macro_Any, Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_KeypadNumLock, \
-    Key_Enter, Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,       \
-               Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,       \
-    ___,       Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,       \
-    OSM(LeftAlt), OSM(LeftGui), Key_Spacebar, Key_Delete,                   \
-    Key_Keymap1_Momentary \
-)
+enum { QWERTY, FUNCTION, NUMPAD };  // layers
 
 const Key keymaps[][ROWS][COLS] PROGMEM = {
-  QWERTY,
-  GENERIC_FN2,
-  NUMPAD
+  [QWERTY] = KEYMAP_STACKED
+  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Macro_LEDToggle,
+   Key_Tab,      Key_A, Key_S, Key_D, Key_F, Key_G,
+   Key_Home,     Key_Z, Key_X, Key_C, Key_V, Key_B, Key_End,
+   Key_Backspace, OSM(LeftShift), OSM(LeftControl), OSL(2),
+   Key_Keymap1_Momentary,
+
+   Macro_Any, Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_KeypadNumLock,
+   Key_Enter, Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
+              Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
+   ___,       Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
+   OSM(LeftAlt), OSM(LeftGui), Key_Spacebar, Key_Delete,
+   Key_Keymap1_Momentary),
+
+  [FUNCTION] = KEYMAP_STACKED
+  (___,      Key_F1,           Key_F2,      Key_F3,        Key_F4,        Key_F5,           XXX,
+   Key_Tab,  Key_mouseBtnR,    Key_mouseUp, Key_mouseBtnL, Key_mouseWarpNW, Key_mouseWarpNE, Key_PageDown,
+   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR,    Key_mouseWarpSW, Key_mouseWarpSE,
+   Key_End,  Key_PrintScreen,  Key_Insert,  Key_mouseBtnM, Key_mouseWarpEnd, ___,            Key_PageUp,
+   Key_Delete, OSM(LeftShift), OSM(LeftControl), ___,
+                                                              ___,
+
+   Consumer_ScanPreviousTrack, Key_F6,                   Key_F7,                   Key_F8,                Key_F9,           Key_F10,        Key_F11,
+   Consumer_PlaySlashPause,    Consumer_ScanNextTrack,   Key_LeftCurlyBracket,     Key_RightCurlyBracket, Key_LeftParen,    Key_RightParen, Key_F12,
+                               Key_LeftArrow,            Key_DownArrow,            Key_UpArrow,           Key_RightArrow,   Key_Pipe,       ___,
+   Consumer_Mute,              Consumer_VolumeIncrement, Consumer_VolumeDecrement, Key_LeftBracket,       Key_RightBracket, Key_Backslash,  Key_Pipe,
+   OSM(LeftAlt), OSM(LeftGui), Key_Enter, Key_Escape,
+   ___),
+
+  [NUMPAD] = KEYMAP_STACKED
+  (___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___,
+   ___,
+
+   Macro_VersionInfo,    ___, Key_Keypad7, Key_Keypad8, Key_Keypad9,   Key_KeypadSubtract, ___,
+   ___,                  ___, Key_Keypad4, Key_Keypad5, Key_Keypad6,   Key_KeypadAdd,      ___,
+                         ___, Key_Keypad1, Key_Keypad2, Key_Keypad3,   Key_KeypadMultiply, Key_Quote,
+   ___,                  ___, Key_Keypad0, Key_Comma,   Key_KeypadDot, Key_KeypadDivide,   Key_Enter,
+   ___, ___, ___, ___,
+   ___)
 };
 
 static kaleidoscope::LEDSolidColor solidYellow(130, 100, 0);
@@ -133,7 +124,7 @@ void setup() {
                    &ActiveModColorEffect,
                    NULL);
 
-  NumLock.numPadLayer = NUMPAD_KEYMAP;
+  NumLock.numPadLayer = NUMPAD;
   LEDRainbowEffect.brightness(120);
   LEDRainbowWaveEffect.brightness(120);
   StalkerEffect.variant = STALKER(BlazingTrail);
