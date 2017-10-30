ab0bd4ed75ec8de07ba3cbde694eb09b27198a4b
Craig Disselkoen <cdisselk@eng.ucsd.edu>
2017-10-29 20:24:29 -0700

    Add Steno layer

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -17,6 +17,7 @@
 #include "Kaleidoscope-Numlock.h"
 #include "Kaleidoscope-OneShot.h"
 #include "Kaleidoscope-Escape-OneShot.h"
+#include "Kaleidoscope-Steno.h"
 
 #include "LED-Off.h"
 #include "Kaleidoscope-LEDEffect-BootGreeting.h"
@@ -35,23 +36,38 @@
 #define MACRO_LEDTOGGLE 3
 #define MACRO_RAPIDFIRECLICK 4
 
-enum { QWERTY, FUNCTION, NUMPAD };  // layers
+enum { STENO, QWERTY, FUNCTION, NUMPAD };  // layers
 
 const Key keymaps[][ROWS][COLS] PROGMEM = {
+  [STENO] = KEYMAP_STACKED
+  (LockLayer(QWERTY),    XXX,   XXX,   XXX,   XXX,   XXX,   S(N6),
+   XXX,    S(N1), S(N2), S(N3), S(N4), S(N5), S(ST1),
+   S(FN),  S(S1), S(TL), S(PL), S(HL), S(ST1),
+   S(PWR), S(S2), S(KL), S(WL), S(RL), S(ST2), S(ST2),
+   S(RE1), S(A), S(O), XXX,
+   ShiftToLayer(FUNCTION),
+
+   S(N7),  XXX,    XXX,   XXX,   XXX,   XXX,   XXX,
+   S(ST3), S(N8),  S(N9), S(NA), S(NB), S(NC), XXX,
+   S(ST3), S(FR),  S(PR), S(LR), S(TR), S(DR),
+   S(ST4), S(ST4), S(RR), S(BR), S(GR), S(SR), S(ZR),
+   XXX, S(E), S(U), S(RE2),
+   ShiftToLayer(FUNCTION)),
+
   [QWERTY] = KEYMAP_STACKED
   (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_MacroRec,
    Key_Tab,      Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_Home,     Key_Z, Key_X, Key_C, Key_V, Key_B, Key_End,
-   Key_Backspace, OSM(LeftShift), OSM(LeftControl), OSL(2),
-   Key_Keymap1_Momentary,
+   Key_Backspace, OSM(LeftShift), OSM(LeftControl), OSL(NUMPAD),
+   ShiftToLayer(FUNCTION),
 
    M(MACRO_ANY),       Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_KeypadNumLock,
    Key_MacroPlay,      Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
                        Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
    M(MACRO_LEDTOGGLE), Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
    OSM(LeftAlt), OSM(LeftGui), Key_Spacebar, Key_Delete,
-   Key_Keymap1_Momentary),
+   ShiftToLayer(FUNCTION)),
 
   [FUNCTION] = KEYMAP_STACKED
   (___,      Key_F1,           Key_F2,      Key_F3,        Key_F4,        Key_F5,           XXX,
@@ -116,6 +132,7 @@ const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
 }
 
 void setup() {
+  Serial.begin(9600);
   Kaleidoscope.setup();
   Kaleidoscope.use(&LEDControl, &LEDOff,
                    &LEDRainbowEffect, &LEDRainbowWaveEffect, &LEDChaseEffect,
@@ -127,6 +144,7 @@ void setup() {
 #endif
                    &NumLock,
 
+                   &GeminiPR,  // Steno
                    &OneShot,
                    &EscapeOneShot,
                    &Macros,
